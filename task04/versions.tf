a53d8a86195347e9c635452351bb72419aba3b5f
singhtarun10 <142771358+singhtarun10@users.noreply.github.com>
2025-05-15 11:37:38 +0530

    versions.tf

--- task04/versions.tf
+++ task04/versions.tf
@@ -11,9 +11,9 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "55cb5f04-d927-4cc6-b4e7-934e3f8639ef"
-  tenant_id       = "61363c43-8420-43ca-8f82-801627e16cdf"
-  client_id       = "da1ad7aa-4f91-406c-9f49-507273792d37"
-  client_secret   = "1538Q~rvlMniDqX.5VUfDMdN4LuvjCEzqnHX4ag6"
+  subscription_id = "55cb5f04-d927-4cc6-b4e7-934e3f863abc"
+  tenant_id       = "61363c43-8420-43ca-8f82-801627e16zxt"
+  client_id       = "da1ad7aa-4f91-406c-9f49-50727379sjdf34"
+  client_secret   = "1538Q~rvlMniDqX.5VUfDMdN4LuvjCEzabcdef"
 
 }
